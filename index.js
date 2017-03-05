989b5857d82e3db70760152fc772215ea90f707a
Kevin Guevara <guevarak1@gmail.com>
2017-03-05 07:12:24 -0500

    Adding message types

--- index.js
+++ index.js
@@ -9,58 +9,142 @@ const app = express()
 app.set('port', (process.env.PORT || 5000))
 
 //allows us to process the data
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //Routes
 
-app.get('/', function(req,res){
-	res.send("Hi i am a chatbot");
+app.get('/', function(req, res) {
+    res.send("Hi i am a chatbot");
 })
 
 let token = "EAACngySG46UBAPbnA5ARqr7bLT5iA4YOGYl3frRBuSpwPpDh2Cj5J6DpJP6E7YmZCLH7ZA3LT2xIzsDdWsqxxM8RtrZAP4Bod1ZA7D13CjPrRxZAGvLe6HQHl0nZCALtZBlhRb4GvpZAOZBQZBvMx87IZAUPPXv647JfZCsqDuXx00MlWgZDZD"
-//Facebook
+    //Facebook
 
-app.get('/webhook/', function(req, res){
+app.get('/webhook/', function(req, res) {
 
-	if(req.query['hub.verify_token'] === "blondiebytes"){
-		res.send(req.query['hub.challenge'])
-	}
-	res.send("Wrong Token");
+    if (req.query['hub.verify_token'] === "blondiebytes") {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong Token");
 })
 
-app.post('/webhook/', function(req,res){
-	let messaging_events = req.body.entry[0].messaging
-	for(let i = 0; i < messaging_events.length; i++){
-		let event = messaging_events[i]
-		let sender = event.sender.id
-		if(event.message && event.message.text){
-			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0,100))
-		}
-	}
-	res.sendStatus(200);
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            decideMessage(sender, text)
+                //sendText(sender, "Text echo: " + text.substring(0,100))
+        }
+        if (event.postback) {
+            let text = JSON.stringify(event.postback)
+            decideMessage(sender, text)
+        }
+    }
+    res.sendStatus(200);
 })
 
-function sendText(sender, text){
-	let messageData = {text: text}
-	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs: {access_token: token},
-		method: "POST",
-		json: {
-			recipient: {id:sender},
-			message: messageData
-		}
-	}, function(error, response, body){
-		if(error){
-			console.log("sending error")
-		}else if(response.body.error){
-			console.log("response body error")
-		}
-	})
+function decideMessage(sender, text1) {
+    let text = text1.toLowerCase()
+    if (text.includes("summer")) {
+        sendImageMessage(sender)
+    } else if (text.includes("winter")) {
+        sendGenericMessage(sender)
+    } else {
+        sendText(sender, "I like fall");
+        sendButtonMessage(sender, "What is your favorite season");
+    }
 }
 
-app.listen(app.get('port'), function(){
-	console.log("running: port")
+function sendText(sender, text) {
+    let messageData = { text: text }
+    sendRequest(sender, messageData)
+}
+
+function sendButtonMessage(sender, text) {
+
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "button",
+                "text": text,
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Summer",
+                    "payload": "summer"
+                }, {
+                    "type": "postback",
+                    "title": "winter",
+                    "payload": "winter"
+                }]
+            }
+        }
+    }
+    sendRequest(sender, messageData)
+}
+
+function sendImageMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "image",
+            "payload": {
+                "url": "https://petersapparel.com/img/shirt.png"
+            }
+        }
+    }
+    sendRequest(sender, messageData)
+}
+
+function sendGenericMessage(sender) {
+    let messageDate = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Winter",
+                    "subtitle": "I love winter",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    },
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://en.wikipedia.org/wiki/Winter",
+                        "title": "More about winter"
+                    }]
+                }]
+            }
+        }
+    }
+}
+
+function sendRequest(sender, messageData) {
+
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: { access_token: token },
+        method: "POST",
+        json: {
+            recipient: { id: sender },
+            message: messageData
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("sending error")
+        } else if (response.body.error) {
+            console.log("response body error")
+        }
+    })
+}
+
+app.listen(app.get('port'), function() {
+    console.log("running: port")
 })
