019e9383ab4ac3c90275817dbd1ce144f1f788f9
Kevin Guevara <guevarak1@gmail.com>
2017-03-17 19:13:40 -0400

    set port 1

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ app.get('/', function (req, res) {
 	res.send('Hello world');
 });
 
-app.listen(5000, function () {
-	console.log('Listening on port 5000');
-});
+app.listen(process.env.PORT || 5000)
 
 // respond to facebook's verification
 app.get('/webhook', function(req, res) {
