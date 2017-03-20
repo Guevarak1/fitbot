acec89d6eb50d7eb7955c402f4879d9f204bf6d2
Kevin Guevara <guevarak1@gmail.com>
2017-03-20 07:40:13 -0400

    refactor after

--- index.js
+++ index.js
@@ -207,12 +207,12 @@ function receivedMessage(event) {
     			{
     				"content_type":"text",
     				"title":"Red",
-    				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_RED"
+    				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_CHEST"
     			},
     			{
     				"content_type":"text",
     				"title":"Green",
-    				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_GREEN"
+    				"payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_LEGS"
     			}
     			]
     		}
